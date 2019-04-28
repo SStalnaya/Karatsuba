f825ddc01d158f97ed60f2f37c324f5e85b8b244
sofia <sofia@debian>
2019-04-27 22:30:11 -0300

    Remove unreachable code

--- karatsuba.h
+++ karatsuba.h
@@ -19,9 +19,6 @@ mp::cpp_int karatsuba(mp::cpp_int a, mp::cpp_int b) {
     return -karatsuba(-a, b);
   }
   if(b < 0) {
-    if(a < 0) {
-      return karatsuba(-a,-b);
-    }
     return -karatsuba(a,-b);
   }
   const unsigned int m = std::min(msb(a), msb(b)) / 2;
