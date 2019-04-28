5489d888edd0cb1008b091d4002617baf18db260
sofia <sofia@debian>
2019-04-27 22:37:44 -0300

    Fix potential namespace collision

--- karatsuba.h
+++ karatsuba.h
@@ -1,8 +1,6 @@
 #include <boost/multiprecision/cpp_int.hpp>
 
-namespace mp = boost::multiprecision;
-
-mp::cpp_int karatsuba(mp::cpp_int a, mp::cpp_int b) {
+boost::multiprecision::cpp_int karatsuba(boost::multiprecision::cpp_int a, boost::multiprecision::cpp_int b) {
   /*
     An implementation of the Karatsuba algorithm.
     Calculates a*b asymptotically faster than the standard algorithm.
@@ -28,13 +26,13 @@ mp::cpp_int karatsuba(mp::cpp_int a, mp::cpp_int b) {
   }
 
   // write a=(a0+a1*2^m) and b=(b0+b1*2^m)
-  const mp::cpp_int a1 = a >> m;
-  const mp::cpp_int a0 = a - (a1 << m);
-  const mp::cpp_int b1 = b >> m;
-  const mp::cpp_int b0 = b - (b1 << m);
+  const boost::multiprecision::cpp_int a1 = a >> m;
+  const boost::multiprecision::cpp_int a0 = a - (a1 << m);
+  const boost::multiprecision::cpp_int b1 = b >> m;
+  const boost::multiprecision::cpp_int b0 = b - (b1 << m);
 
-  const mp::cpp_int z0 = karatsuba(a0, b0);
-  const mp::cpp_int z2 = karatsuba(a1, b1);
-  const mp::cpp_int z1 = karatsuba(a1 + a0, b1 + b0) - z2 - z0;
+  const boost::multiprecision::cpp_int z0 = karatsuba(a0, b0);
+  const boost::multiprecision::cpp_int z2 = karatsuba(a1, b1);
+  const boost::multiprecision::cpp_int z1 = karatsuba(a1 + a0, b1 + b0) - z2 - z0;
   return (z2 << (2 * m)) + (z1 << m) + z0;
 }
